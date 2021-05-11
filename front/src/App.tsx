23e4f408f026b3348f4f3c74870b4902afa55132
s-justina <jus.sobcz@gmail.com>
2021-05-11 18:24:50 +0200

    fix conflicts

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
 import BookList from "./components/bookList";
-import React from "react";
-import "./App.css";
 import ShoppingCartButton from "./components/shoppingCartButton";
 
 function App() {
