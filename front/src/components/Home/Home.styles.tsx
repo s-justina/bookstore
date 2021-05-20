43239bc2a64caa14e23d4860585cc03418dce06c
s-justina <jus.sobcz@gmail.com>
2021-05-20 12:43:31 +0200

    navigation styles

--- front/src/components/Home/Home.styles.tsx
+++ front/src/components/Home/Home.styles.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const HomeWrapper = styled.div`
-display: flex;
-flex-direction: column;
-margin-top: 5rem;
+  display: flex;
+  flex-direction: column;
+  margin-top: 3rem;
 `;
